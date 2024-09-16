e32656a7b3ace41ba41def9f1eb1b1c1136b8e11
Mykhailo Turchyn <ktcsmart1711@gmail.com>
2024-09-16 23:11:04 +0300

    added connection to mongoDB via mongoose

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const tasks_api = require('./routes/tasks_api');
 const app = express();
+const connectDB = require('./dataBase/connect')
+require('dotenv').config();
 
 app.use(express.json());
 app.use('/api/v1/tasks', tasks_api);
 
-const PORT = 3000;
-app.listen(PORT, ()=>{console.log('listening on port 3000....')});
+const PORT = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, ()=>{console.log('listening on port 3000....')});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
